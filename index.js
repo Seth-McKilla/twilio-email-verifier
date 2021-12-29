49d82fd266be8bae6d9056eb64d4239b050f5dd9
Seth-McKilla <seth@endevrs.dev>
2021-12-29 08:11:46 -0500

    Finish email validation script

--- index.js
+++ index.js
@@ -1,31 +1,72 @@
-"use strict";
-const fs = require("fs");
+const path = require("path");
+const sleep = require("util").promisify(setTimeout);
+const csv = require("fast-csv");
 require("dotenv").config();
 const client = require("@sendgrid/client");
 client.setApiKey(process.env.SENDGRID_API_KEY);
-const emails = require("./emails");
 
-(async () => {
+const inputFile = path.resolve(
+  __dirname,
+  "assets",
+  "DAO_central_emails_1-500.csv"
+);
+const outputFile = path.resolve(
+  __dirname,
+  "assets",
+  "DAO_central_emails_validated.csv"
+);
+
+function readCsv(path, options, rowProcessor) {
+  return new Promise((resolve, reject) => {
+    const data = [];
+
+    csv
+      .parseFile(path, options)
+      .on("error", reject)
+      .on("data", (row) => {
+        const obj = rowProcessor(row);
+        if (obj) data.push(obj);
+      })
+      .on("end", () => {
+        resolve(data);
+      });
+  });
+}
+
+(async function () {
   let results = [];
 
-  await Promise.all(
-    emails.map(async (email) => {
-      const data = {
-        email,
+  const data = await readCsv(inputFile, { skipRows: 1 }, (row) => ({
+    submission_id: row[0],
+    respondent_id: row[1],
+    submitted_at: row[2],
+    first_name: row[3],
+    ens_address: row[4],
+    email: row[5],
+  }));
+
+  return await Promise.all(
+    data.map(async (item) => {
+      const body = {
+        email: item.email,
         "source": "signup",
       };
-
       try {
         const [response] = await client.request({
           url: `/v3/validations/email`,
           method: "POST",
-          body: data,
+          body,
         });
-        const result = response.body.result;
-        results.push(result);
+        const { result } = response.body;
+        await sleep(150); // sendgrid rate limited to 7 requests per second
+        results.push({
+          ...item,
+          verdict: result.verdict,
+          score: result.score,
+        });
       } catch (error) {
         console.error(error);
       }
     })
-  ).then(() => fs.writeFileSync("results.json", JSON.stringify(results)));
+  ).then(() => csv.writeToPath(outputFile, results, { headers: true }));
 })();
